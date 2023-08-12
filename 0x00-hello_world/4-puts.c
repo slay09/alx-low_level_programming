2cc811e003bcaf48689e3c772a2fb9627cdffdf2
Othmane Hadji <hadjiothmanecs@gmail.com>
2023-08-12 11:05:38 -0700

    hellov2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry poit
-*return: always 0 (success)
-*/
+ * main - Entry point
+ *
+ * This function is the entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
