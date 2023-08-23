aaf5126178ff4103f4f27f63630469d3ea2762bb
Othmane Hadji <hadjiothmanecs@gmail.com>
2023-08-23 10:34:07 -0700

    remaining_task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,14 @@
 
 void reverse_array(int *a, int n)
 {
+
 	int tmp, index;
 
 	for (index = n − 1; index >= n / 2; index--)
 	{
-		tmp = a[n 1 - index];
-		a[n 1 - index] = a[index];
-		a[index] = tmp;
+	tmp = a[n - 1 - index];
+	a[n - 1 - index] = a[index];
+	a[index] = tmp;
 	}
 }
 
